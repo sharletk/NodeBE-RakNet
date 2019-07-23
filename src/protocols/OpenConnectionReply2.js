2b57ca19db46bb617b87510017c62d9d285ac944
SharletP <sharletman@gmail.com>
2019-07-23 10:47:45 +0300

    Added OpenConnectionReply2 Protocol

---
+++ src/protocols/OpenConnectionReply2.js
@@ -0,0 +1,36 @@
+"use strict"
+
+const OfflineMessage = require("./OfflineMessage.js");
+
+const MessageIdentifiers = require("./MessageIdentifiers.js");
+
+class OpenConnectionReply2 extends OfflineMessage {
+  constructor() {
+    super();
+    
+    this.ID = MessageIdentifiers.ID_OPEN_CONNECTION_REPLY_2;
+    
+    this.serverID;
+    this.clientAddress;
+    this.serverSecurity = false;
+    this.mtuSize;
+  }
+  
+  encodePayload() {
+    this.writeMagic();
+    this.writeLong(this.serverID);
+    //Address
+    this.writeShort(this.mtuSize);
+    this.writeByte(this.serverSecurity ? 1 : 0);
+  }
+  
+  decodePayload() {
+    this.readMagic();
+    this.serverID = this.readLong();
+    //Address
+    this.mtuSize = this.readShort();
+    this.serverSecurity = this.readByte() !== 0;
+  }
+}
+
+module.exports = OpenConnectionReply2;

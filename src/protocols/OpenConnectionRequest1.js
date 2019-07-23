9c96603a95524c7a14c59c1417e5cf43257efe88
SharletP <sharletman@gmail.com>
2019-07-23 11:02:06 +0300

    Added OpenConnectionRequest1 Protocol

---
+++ src/protocols/OpenConnectionRequest1.js
@@ -0,0 +1,31 @@
+"use strict"
+
+const OfflineMessage = require("./OfflineMessage.js");
+
+const MessageIdentifiers = require("./MessageIdentifiers.js");
+
+class OpenConnectionRequest1 extends OfflineMessage {
+  constructor() {
+    super();
+    
+    this.ID = MessageIdentifiers.ID_OPEN_CONNECTION_REQUEST_1;
+        
+    this.protocol = 9;
+    
+    this.mtuSize;
+  }
+  
+  encodePayload() {
+    this.writeMagic();
+    this.writeByte(this.mtuSize);
+  }
+  
+  decodePayload() {
+    this.readMagic();
+    this.protocol = this.readByte();
+    this.mtuSize = this.length;
+    this.getRemainingBytes();
+  }
+}
+
+module.exports = OpenConnectionRequest1;

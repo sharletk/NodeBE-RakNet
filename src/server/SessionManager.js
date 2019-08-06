6e4207c909eb9db0e0a7051784d83f2931970d6b
SharletP <sharletman@gmail.com>
2019-08-06 15:19:41 +0300

    Added SessionManager

---
+++ src/server/SessionManager.js
@@ -0,0 +1,468 @@
+"use strict"
+
+const BinaryStream = require("nodebe-binarystream");
+
+const Packet = require("../protocol/Packet.js");
+const EncapsulatedPacket = require("../protocol/EncapsulatedPacket.js");
+const Datagram = require("../protocol/Datagram.js");
+const ACK = require("../protocol/ACK.js");
+const NACK = require("../protocol/NACK.js");
+
+const OfflineMessageHandler = require("./OfflineMessageHandler.js");
+
+const ITCProtocol = require("./ITCProtocol.js");
+
+const RakNet = require("../NodeBERakNet.js");
+
+const Session = require("./Session.js");
+
+const Methods = require("nodebe-methods");
+
+class SessionManager {
+  constructor(server, socket, maxMtuSize) {
+    this.RAKNET_TPS = 100;
+    this.RAKNET_TIME_PER_TICK = 1 / this.RAKNET_TPS;
+    
+    this.server = server;
+    this.socket = socket;
+    
+    this.binarystream = new BinaryStream();
+    
+    this.maxMtuSize = maxMtuSize;
+    
+    this.startTimeMS = Date.now();
+    
+    this.offlineMessageHandler = new OfflineMessageHandler(this, this.binarystream);
+    
+    this.reuseableAddress = this.socket.getBindAddress();
+    
+    this.recieveBytes = 0;
+    this.sendBytes = 0;
+    
+    this.sessionsByAddress = new Map();
+    
+    this.sessions = new Map();
+    
+    this.name = "";
+    
+    this.packetLimit = 200;
+    
+    this.shutdown = false;
+    
+    this.ticks = 0;
+    this.lastMeasure;
+    
+    this.block = new Map();
+    this.ipSec = new Map();
+    
+    this.rawPacketFilters = new Map();
+    
+    this.portChecking = false;
+    
+    this.nextSessionId = 0;
+    
+    this.socket.getSocket().on("message", (msg, rinfo) => {
+        this.getLogger().log(`MSG: ${msg} \n RINFO: ${JSON.stringify(rinfo)}`);
+        
+        this.binarystream.writeData(msg);
+        
+        this.socket.msg = msg;
+        this.socket.rinfo = rinfo;
+    });
+  }
+  
+  getRakNetTimeMS() {
+    return Number(Date.now() * 1000 - this.startTimeMS);
+  }
+  
+  getPort() {
+    return this.socket.getBindAddress().port;
+  }
+  
+  getMaxMtuSize() {
+    return this.maxMtuSize;
+  }
+  
+  getProtocolVersion() {
+    return this.server.getProtocolVersion();
+  }
+  
+  getLogger() {
+    return this.server.getLogger();
+  }
+  
+  run() {
+    this.tickProcessor();
+  }
+  
+  tickProcessor() {
+    this.lastMeasure = Date.now();
+    
+    let tick = setInterval(() => {
+      if (!this.shutdown) {
+        let start = Date.now();
+        
+        let i;
+        let stream;
+        let socket;
+        
+        for (stream = true, i = 0; i < 100 && stream && !this.shutdown; ++i) {
+          stream = this.recieveStream();
+        }
+        
+        for (socket = true, i = 0; i < 100 && socket && !this.shutdown; ++i) {
+          socket = this.recievePacket();
+        }
+        
+        this.tick();
+      } else {
+        clearInterval(tick);
+      }
+    }, this.RAKNET_TIME_PER_TICK * 1000);
+  }
+  
+  tick() {
+    let time = Date.now();
+    for (let session of this.sessions.keys()) {
+      if (this.sessions.size > 1) {
+        console.log(this.sessions.size)
+        session.update(time);
+     }
+    }
+    
+    this.ipSec = new Map();
+    
+    if ((this.ticks & this.RAKNET_TPS) === 0) {
+      if (this.sendBytes > 0 || this.recieveBytes > 0) {
+        let diff = Math.max(0.005, time - this.lastMeasure)
+        
+        let bandwidth = {
+          up: this.sendBytes / diff,
+          down: this.recieveBytes / diff
+        };
+        this.streamOption("bandwidth", bandwidth);
+				
+				this.sendBytes = 0;
+				this.recieveBytes = 0;
+      }
+      this.lastMeasure = time;
+      
+      if (this.block.length > 0) {
+        //asort(this.block);
+        let now = Date.now();
+        for (let timeout in this.block) {
+          if (timeout <= now) {
+            for (let address in this.block)
+            this.block.delete(address);
+          } else {
+            break;
+          }
+        }
+      }
+    }
+    ++this.ticks;
+  }
+  
+  recievePacket() {
+    let address = this.reuseableAddress;
+    let stream = this.binarystream;
+    
+    let len = stream.buffer.length;
+    
+    /*if (len === false) {
+      //implement error thingy.
+    }*/
+    
+    this.recieveBytes += len;
+    
+    if(this.block.has(address.ip)) return true;
+    
+    if (this.ipSec.has(address.ip)) {
+      if (++this.ipSec.get(address.ip) >= this.packetLimit) {
+        this.blockAddress(address.ip);
+        return true;
+      }
+    } else {
+      this.ipSec.set[address.ip, 1];
+    }
+    
+    if (len < 1) return true;
+    
+    try {
+      let session = this.getSessionByAddress(address);
+      
+      if (session !== null) {
+        let header = stream.readByte();
+        if ((header & Datagram.BITFLAG_VALID) !== 0) {
+          if (header & Datagram.BITFLAG_ACK) {
+            session.handlePacket(new ACK(buffer));
+          } else if (header & Datagram.BITFLAG_NAK) {
+            session.handlePacket(new NACK(buffer));
+          } else {
+            session.handlePacket(new Datagram(buffer));
+          }
+        } else {
+          this.getLogger().debug(`Ignored unconnected packet from ${address} due to session already opened (${stream.toHex(stream.readByte)})`);
+        }
+      } else if (this.offlineMessageHandler.handleRaw(stream, address)) {
+        let handled = false;
+        for (pattern of this.rawPacketFilters) {
+          if (preg_match(pattern, stream) > 0) {
+            let handled = true;
+            this.streamRaw(address, stream);
+            break;
+          }
+        }
+        
+        if (!handled) {
+          this.getLogger().debug(`Ignored packet from ${address} due to no session opened (${stream.readByte()})`);
+        }
+      }
+    } catch (error) {
+      this.getLogger().error(error);
+      // IMPLEMENT A PROPER ERROR SYSTEM.
+      
+      this.blockAddress(address.ip, 5);
+    }
+    
+    return true;
+  }
+  
+  sendPacket(packet, address) {
+    packet.encode;
+    this.sendBytes += this.socket.writePacket(packet.getBuffer(), address.ip, address.port);
+  }
+  
+  streamEncapsulated(session, packet, flags = RakNet.PRIORITY_NORMAL) {
+    let buffer = String.fromCharCode(ITCProtocol.PACKET_ENCAPSULATED) + this.binarystream.writeInt(session.getInternalId) + String.fromCharCode(flags) + packet.toInternalBinary();
+    
+    this.server.pushThreadToMainPacket(buffer);
+  }
+  
+  streamRaw(source, payload) {
+    let buffer = String.fromCharCode(ITCProtocol.PACKET_RAW) + String.fromCharCode(source.ip.length) + source.ip + this.binarystream.writeShort(source.port) + payload;
+    
+    this.server.pushThreadToMainPacket(buffer);
+  }
+  
+  streamClose(identifier, reason) {
+    let buffer = String.fromCharCode(ITCProtocol.PACKET_CLOSE_SESSION) + this.binarystream.writeInt(identifier) + String.fromCharCode(reason.length) + reason;
+    
+    this.server.pushThreadToMainPacket(buffer);
+  }
+  
+  streamInvalid(identifier) {
+    let buffer = String.fromCharCode(ITCProtocol.PACKET_INVALID_SESSION) + this.binarystream.writeInt(identifier);
+    
+    this.server.pushThreadToMainPacket(buffer);
+  }
+  
+  streamOpen(session) {
+    let address = session.getAddress();
+    
+    this.getLogger.debug(address);
+    
+    let buffer = String.fromCharCode(ITCProtocol.PACKET_OPEN_SESSION) + this.binarystream.writeInt(session.getInternalId()) + String.fromCharCode(address.ip.length) + address.ip + this.binarystream.writeShort(address.port) + this.binarystream.writeLong(session.getID());
+    
+    this.server.pushThreadToMainPacket(buffer);
+  }
+  
+  streamACK(identifier, identifierACK) {
+    let buffer = String.fromCharCode(ITCProtocol.PACKET_ACK_NOTIFICATION) + this.binarystream.writeInt(identifier) + this.binarystream.writeInt(identifierACK);
+    
+    this.server.pushThreadToMainPacket(buffer);
+  }
+  
+  streamOption(name, value) {
+    let buffer = String.fromCharCode(ITCProtocol.PACKET_SET_OPTION) + String.fromCharCode(name.length) + name + value;
+    
+    this.server.pushThreadToMainPacket(buffer);
+  } 
+  
+  streamPingMeasure(session, pingMS) {
+    let buffer = String.fromCharCode(ITCProtocol.PACKET_REPORT_PING) + this.binarystream.writeInt(session.getInternalId()) + this.binarystream.writeInt(pingMS);
+    
+    this.server.pushThreadToMainPacket(buffer);
+  }
+  
+  recieveStream() {
+    let packet = this.server.readMainToThreadPacket();;
+    if(packet !== null) {
+      let id = 0;
+      let offset = this.binarystream.offset;
+      offset = 1;
+      if (id === ITCProtocol.PACKET_ENCAPSULATED) {
+        let identifier = this.binarystream.readInt();
+        offset += 4;
+        let session = this.sessions.get(identifier) || null;
+        if (session !== null && session.isConnected()) {
+          let flags;
+          let buffer = this.binarystream.buffer;
+          session.addEncapsulatedToQueue(EncapsulatedPacket.fromInternalBinary(buffer), flags);
+        } else {
+          this.streamInvalid(identifier);
+        }
+      } else if (id === ITCProtocol.PACKET_RAW) {
+        let len = packet[offset++].charCodeAt();
+        let address = packet.substring(offset, len);
+        offset += len;
+        let port = this.BinaryStream.readShort(packet.substring(offset, 2));
+        offset += 2;
+        let payload = substr($packet, $offset);
+        this.socket.writePacket(payload, address, port)
+      } else if (id === ITCProtocol.PACKET_CLOSE_SESSION) {
+        let identifier = this.BinaryStream.readInt(packet.substring(offset, 4));
+        if (Methods.Isset(this.sessions.get(identifier))) {
+          this.sessions.get(identifier).flagForDisconnection();
+        } else {
+          this.streamInvalid(identifier);
+        }
+      } else if (id === ITCProtocol.PACKET_INVALID_SESSION) {
+        let identifier = this.BinaryStream.readInt(packet.substring(offset, 4));
+        if (Methods.Isset(this.sessions.get(identifier))) {
+          this.removeSession(this.sessions.get(identifier));
+        }
+      } else if (id === ITCProtocol.PACKET_SET_OPTION) {
+        let len = packet[offset++].charCodeAt();
+        let name = packet.substring(offset, len);
+        offset += len;
+        let value = packet.substring(offset);
+        
+        switch (name) {
+          case "name":
+          this.name = value;
+          break;
+          
+          case "portChecking":
+          this.portChecking = value;
+          break;
+          
+          case "packetLimit":
+          this.packetLimit = value;
+          break;
+        }
+      } else if (id === ITCProtocol.PACKET_BLOCK_ADDRESS) {
+        let len = packet[offset++].charCodeAt();
+        let address = paclet.substring(offset, len);
+        offset += len;
+        let timeout = this.BinaryStream.readInt(packet.substring(offset, 4));
+        this.blockAddress(address, timeout);
+      } else if (id === ITCProtocol.PACKET_UNBLOCK_ADDRESS) {
+        let len = packet[offset++].charCodeAt();
+        let address = packet.substring(offset, len);
+        this.unblockAddress(address);
+      } else if (id === ITCProtocol.PACKET_RAW_FILTER) {
+        let pattern = packet.substring(offset);
+        this.rawPacketFilters = pattern;
+      } else if (id === ITCProtocol.PACKET_SHUTDOWN) {
+        for (session of this.sessions) {
+          this.removeSession(session);
+        }
+        
+        this.socket.close();
+        this.shutdown = true;
+      } else if (id === ITCProtocol.PACKET_EMERGENCY_SHUTDOWN) {
+        this.shutdown = true;
+      } else if (id === 0) {
+        return;
+      }
+      else {
+        this.getLogger().debug(`Unknown RakLib internal packet (ID 0x${id.toString(16)} received from main thread`);
+      }
+           
+      return true;
+    }
+    
+    return false;
+  }
+  
+  blockAddress(address, timeout = 300) {
+    let final = Date.now() + timeout;
+    
+    if (!this.block.has(address) || timeout !== 1) {
+      if (timeout === -1) {
+        final = Number.MAX_SAFE_INTEGER;
+      } else {
+        this.getLogger().notice(`Blocked ${address} for ${timeout} seconds.`);
+      }
+      this.blockAddress[address] = final;
+    } else if (this.block.get(address) < final) {
+      this.block.set(address, final);
+    }
+  }
+  
+  unblockAddress(address) {
+    this.block.delete(address);
+    this.getLogger().debug(`Unblocked ${address}`);
+  }
+  
+  getSessionByAddress(address) {
+    return this.sessionsByAddress[address.toString()] || null;
+  }
+  
+  sessionExists(address) {
+    return Methods.Isset(this.sessionsByAddress.get(address.toString()));
+  }
+  
+  createSession(address, logger, clientId, mtuSize) {
+    this.checkSessions();
+    
+    while(Methods.Isset(this.sessions.get(this.nextSessionId))) {
+      this.nextSessionId++;
+      this.nextSessionId &= 0x7fffffff;
+    }
+    
+    let session = new Session(this, logger, address, clientId, mtuSize, this.nextSessionId);
+    
+    this.sessionsByAddress.set(address.ip, session);
+    this.sessions.set(this.nextSessionId, session);
+    this.getLogger().debug(`Created session for ${address.ip} with MTU size ${mtuSize}`);
+    
+    return session;
+  }
+  
+  removeSession(session, reason = "unknown") {
+    let id = session.getInternalId();
+    if(Methods.Isset(this.sessions.get(id))) {
+      this.sessions.get(id).close();
+      this.removeSessionInternal(session);
+      this.streamClose(id, reason);
+    }
+  }
+  
+  removeSessionInternal(session) {
+    this.sessionsByAddress.delete(session.getAddress().ip);
+    this.sessions.delete(session.getInternalId());   
+  }
+  
+  openSession(session) {
+    this.streamOpen(session);
+  }
+  
+  checkSessions() {
+    if(this.sessions.size > 4096) {
+      for (session of this.sessions) {
+        if (session.isTemporal()) {
+          this.removeSessionInternal(session);
+          if (this.sessions.size <= 4096) {
+            break;
+          }
+        }
+      }
+    }
+  }
+  
+  notifyACK(session, identifierACK) {
+    this.streamACK(session.getInternalId(), identifierACK);
+  }
+  
+  getName() {
+    return this.name;
+  }
+  
+  getID() {
+    return this.server.getServerId();
+  }
+}
+
+module.exports = SessionManager;
